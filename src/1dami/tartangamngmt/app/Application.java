edf30964c5ea0a970cb72fdfaf047702d90ef1b1
Daniel Negrete <daninc2003@gmail.com>
2023-03-14 09:43:03 +0100

    First commit: Application and LoginController scaffolds

---
+++ src/1dami/tartangamngmt/app/Application.java
@@ -0,0 +1,13 @@
+package 1dami.tartangamngmt.app;
+/**
+* Main class for the Tartanga's management application
+*/
+public class Application{
+	/**
+	* Entry point for the application
+	*/
+	public static void main (String args){
+	//this method does nothing
+	}
+
+}
